1bcabe2d6a9da026379be1ec25c58f4dcc197a0c
赵尉尉 <4332weizi@gmail.com>
2016-10-11 17:19:05 +0800

    数据更新

--- data/supermarkets.js
+++ data/supermarkets.js
@@ -3,7 +3,7 @@ function initSupermarkets() {
         {
             title: "家乐福",
             config: {
-                divName: 'metro',
+                divName: 'supermarket',
                 circleColor: 'fuchsia',
                 circleRadius: 3000,
                 title: '超'
@@ -22,6 +22,170 @@ function initSupermarkets() {
                     "position": [113.665849, 34.810696]
                 }
             ]
+        },
+        {
+            title: "万达广场",
+            config: {
+                divName: 'supermarket',
+                circleColor: 'fuchsia',
+                circleRadius: 5000,
+                title: '商'
+            },
+            datas: [
+                {
+                    "name": "二七万达广场",
+                    "position": [113.64303699999999, 34.718373]
+                },
+                {
+                    "name": "万达广场(郑州中原店)",
+                    "position": [113.60150299999998, 34.746419]
+                },
+                {
+                    "name": "惠济万达广场(建设中)",
+                    "position": [113.636349, 34.868529]
+                }
+
+            ]
+        },
+        {
+            title: "沃尔玛",
+            config: {
+                divName: 'supermarket',
+                circleColor: 'fuchsia',
+                circleRadius: 3000,
+                title: '超'
+            },
+            datas: [
+                {
+                    "name": "沃尔玛(金水路店)",
+                    "position": [113.70470599999999, 34.761098]
+                },
+                {
+                    "name": "沃尔玛(大学路店)",
+                    "position": [113.644833, 34.724281]
+                },
+                {
+                    "name": "沃尔玛(中原西路店)",
+                    "position": [113.600862, 34.746461]
+                }
+
+            ]
+        },
+        {
+            title: "丹尼斯",
+            config: {
+                divName: 'supermarket',
+                circleColor: 'fuchsia',
+                circleRadius: 3000,
+                title: '超'
+            },
+            datas: [
+                {
+                    "name": "丹尼斯百货人民路店",
+                    "position": [113.675142, 34.757059]
+                },
+                {
+                    "name": "丹尼斯百货花园路店",
+                    "position": [113.682398,34.787107]
+                },
+                {
+                    "name": "丹尼斯CBD七天地店",
+                    "position": [113.722267,34.777813]
+                },
+                {
+                    "name": "丹尼斯中原店",
+                    "position": [113.628447, 34.757976]
+                },
+                {
+                    "name": "丹尼斯郑花店",
+                    "position": [113.682369,34.801011]
+                },
+                {
+                    "name": "丹尼斯京广店",
+                    "position": [113.651623,34.749224]
+                },
+                {
+                    "name": "丹尼斯航海店",
+                    "position": [113.739112,34.721529]
+                },
+                {
+                    "name": "丹尼斯郑州大学店",
+                    "position": [113.682398,34.787107]
+                },
+                {
+                    "name": "丹尼斯丰乐店",
+                    "position": [113.642863,34.791275]
+                }
+            ]
+        },
+        {
+            title: "王府井百货",
+            config: {
+                divName: 'supermarket',
+                circleColor: 'fuchsia',
+                circleRadius: 5000,
+                title: '商'
+            },
+            datas: [
+                {
+                    "name": "王府井百货",
+                    "position": [113.61229600000001, 34.76128635]
+                },
+                {
+                    "name": "王府井百货",
+                    "position": [113.71751899999998, 34.784815]
+                },
+                {
+                    "name": "新王府井百货(建设中)",
+                    "position": [113.71846299999999, 34.783076]
+                }
+
+            ]
+        },
+        {
+            title: "锦艺城购物中心",
+            config: {
+                divName: 'supermarket',
+                circleColor: 'fuchsia',
+                circleRadius: 5000,
+                title: '商'
+            },
+            datas: [
+                {
+                    "name": "锦艺城购物中心",
+                    "position": [113.611454, 34.761848]
+                }
+            ]
+        },
+        {
+            title: "曼哈顿商业广场",
+            config: {
+                divName: 'supermarket',
+                circleColor: 'fuchsia',
+                circleRadius: 5000,
+                title: '商'
+            },
+            datas: [
+                {
+                    "name": "曼哈顿商业广场",
+                    "position": [113.706851, 34.762187]
+                }
+            ]
+        },
+        {
+            title: "郑州国贸360广场",
+            config: {
+                divName: 'supermarket',
+                circleColor: 'fuchsia',
+                circleRadius: 5000,
+                title: '商'
+            },
+            datas: [
+                {
+                    "name": "郑州国贸360广场",
+                    "position": [113.680034, 34.7847]
+                }
+            ]
         }
-    ];
+    ]
 }
